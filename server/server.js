2e7640c7d89fd20d4fe483b42ae518c708c26052
carlos benavides <1992cab@gmail.com>
2023-03-06 18:49:58 -0500

    chore: updated deps

--- server/server.js
+++ server/server.js
@@ -1,29 +1,34 @@
-require('./config/config')
-const express = require('express')
-const mongoose = require('mongoose');
-const path = require('path');
+require("./config/config");
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // routes
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 // enable public folder
-app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.static(path.resolve(__dirname, "../public")));
 
-mongoose.connect(process.env.DB, {
+mongoose.set("strictQuery", true);
+
+mongoose.connect(
+  process.env.DB,
+  {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    useCreateIndex: true
-}, (err, res) => {
+  },
+  (err, res) => {
     if (err) throw err;
-    console.log('Database online');
-});
+    console.log("Database online");
+  }
+);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`)
+  console.log(`Listening on port ${process.env.PORT}`);
 });
